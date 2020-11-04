15c54cb46b58df90d5590323c29855859bf72fa8
Khalid War <git@khalidwar.com>
2020-11-04 11:24:36 -0600

    Improved create new alias
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -129,76 +129,13 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   FloatingActionButton buildFloatingActionButton(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return FloatingActionButton(
       child: Icon(Icons.add),
       onPressed: () {
-        TextEditingController textFieldController = TextEditingController();
         showModalBottomSheet(
             context: context,
             builder: (context) {
-              return Container(
-                height: size.height * 0.6,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Divider(
-                          thickness: 3,
-                          color: Colors.grey,
-                          indent: size.width * 0.35,
-                          endIndent: size.width * 0.35,
-                        ),
-                        SizedBox(height: size.height * 0.01),
-                        TextField(
-                          controller: textFieldController,
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10)),
-                            ),
-                            hintText: 'Description (optional)',
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        DomainFormatWidget(
-                            label: 'Domain:', value: '@anonaddy.me'),
-                        SizedBox(height: size.height * 0.03),
-                        DomainFormatWidget(label: 'Format:', value: 'UUID'),
-                      ],
-                    ),
-                    Container(
-                      width: double.infinity,
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          'Generate New Alias',
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        onPressed: () {
-                          if (textFieldController.text.isEmpty ||
-                              textFieldController.text == null) {
-                            _apiDataManager.createNewAlias(
-                                description: 'No Description Provided');
-                          }
-                          _apiDataManager.createNewAlias(
-                              description: textFieldController.text.toString());
-                          Navigator.pop(context);
-                          setState(() {
-                            _refreshData();
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              );
+              return CreateNewAlias(apiCallManager: _apiCallManager);
             });
       },
     );
