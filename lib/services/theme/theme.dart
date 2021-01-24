58c4d46ac988876b27cf49368b405529707cabfe
Khalid War <git@khalidwar.com>
2021-01-23 19:41:19 -0600

    Fixed several theme color issues
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/services/theme/theme.dart
+++ lib/services/theme/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
-ThemeData lightTheme = ThemeData(
+ThemeData lightTheme = ThemeData.light().copyWith(
   brightness: Brightness.light,
   primaryColor: Colors.white,
   primaryColorLight: Colors.white,
@@ -11,21 +11,27 @@ ThemeData lightTheme = ThemeData(
   toggleableActiveColor: Color(0xFF62F4EB),
   accentColor: kAccentColor,
   buttonTheme: buttonThemeData,
+  floatingActionButtonTheme: fabThemeData,
   cardTheme: cardTheme,
   dialogTheme: dialogTheme,
 );
 
-ThemeData darkTheme = ThemeData(
+ThemeData darkTheme = ThemeData.dark().copyWith(
   brightness: Brightness.dark,
+  scaffoldBackgroundColor: Colors.black,
   primaryColor: Colors.black,
   primaryColorLight: Color(0xFF0d0d0d),
   primaryColorDark: Colors.black,
   appBarTheme: appBarTheme,
   toggleableActiveColor: kAccentColor,
   accentColor: kAccentColor,
+  dividerColor: Colors.grey[600],
   buttonTheme: buttonThemeData,
-  cardTheme: cardTheme.copyWith(color: Color(0xFF0d0d0d)),
+  floatingActionButtonTheme: fabThemeData,
+  cardTheme: cardTheme.copyWith(color: Color(0xFF141414)),
   dialogTheme: dialogTheme.copyWith(backgroundColor: Color(0xFF0f0f0f)),
+  bottomNavigationBarTheme:
+      BottomNavigationBarThemeData(backgroundColor: Colors.black),
 );
 
 ButtonThemeData buttonThemeData = ButtonThemeData().copyWith(
@@ -36,15 +42,13 @@ ButtonThemeData buttonThemeData = ButtonThemeData().copyWith(
 );
 
 CardTheme cardTheme = CardTheme().copyWith(
-  elevation: 6,
   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
   margin: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
 );
 
 DialogTheme dialogTheme = DialogTheme().copyWith(
-  backgroundColor: Colors.white,
   shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(16),
+    borderRadius: BorderRadius.circular(10),
     side: BorderSide(style: BorderStyle.solid, color: kBlueNavyColor),
   ),
 );
@@ -52,4 +56,10 @@ DialogTheme dialogTheme = DialogTheme().copyWith(
 AppBarTheme appBarTheme = AppBarTheme().copyWith(
   color: kBlueNavyColor,
   brightness: Brightness.dark,
+);
+
+FloatingActionButtonThemeData fabThemeData =
+    FloatingActionButtonThemeData().copyWith(
+  backgroundColor: Color(0xFF62F4EB),
+  foregroundColor: Colors.black,
 );
