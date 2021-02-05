9c6d6d6a76cd83af1dfff04b6b828c12b57ea0ad
Khalid War <git@khalidwar.com>
2021-02-04 23:39:59 -0600

    Fixed bottomModalSheet expansion issue Updated version name and code for release
    Signed-off-by: Khalid War <git@khalidwar.com>

---
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
