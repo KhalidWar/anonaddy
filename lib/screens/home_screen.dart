54e402957c0698a01ccaf9b9492e9e8c6b696773
Khalid War <git@khalidwar.com>
2021-03-26 14:08:30 -0500

    Redesigned offline indicator UI
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:anonaddy/services/connectivity/connectivity_service.dart';
 import 'package:anonaddy/services/search/search_service.dart';
 import 'package:anonaddy/state_management/alias_state_manager.dart';
 import 'package:anonaddy/state_management/providers.dart';
+import 'package:anonaddy/widgets/no_internet_alert.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -39,80 +40,90 @@ class _HomeScreenState extends State<HomeScreen> {
       value: SystemUiOverlayStyle(
         systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      child: Scaffold(
-        appBar: buildAppBar(context),
-        body: Consumer(
-          builder: (_, watch, child) {
-            final connectivityAsyncValue = watch(connectivityStreamProvider);
+      child: Consumer(
+        builder: (_, watch, __) {
+          final connectivityAsyncValue = watch(connectivityStreamProvider);
+          bool isOffline = false;
 
-            connectivityAsyncValue.whenData((data) {
-              if (data == ConnectionStatus.offline) {
-                Fluttertoast.showToast(
-                  msg: 'No Internet. Activated Offline mode.',
-                  toastLength: Toast.LENGTH_LONG,
-                  gravity: ToastGravity.BOTTOM,
-                  backgroundColor: Colors.grey[600],
-                );
-              }
-            });
-
-            return child;
-          },
-          child: IndexedStack(
-            index: _selectedIndex,
-            children: [
-              AccountTab(),
-              AliasTab(),
-              MoreTab(),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: _selectedTab,
-          currentIndex: _selectedIndex,
-          selectedItemColor: isDark ? kAccentColor : kBlueNavyColor,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Account',
+          connectivityAsyncValue.whenData((data) {
+            if (data == ConnectionStatus.offline) {
+              isOffline = true;
+            } else {
+              isOffline = false;
+            }
+          });
+          return Scaffold(
+            appBar: buildAppBar(context, isOffline),
+            body: IndexedStack(
+              index: _selectedIndex,
+              children: [
+                AccountTab(),
+                AliasTab(),
+                MoreTab(),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.alternate_email_sharp),
-              label: 'Aliases',
+            bottomNavigationBar: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                isOffline ? NoInternetAlert() : Container(),
+                BottomNavigationBar(
+                  onTap: _selectedTab,
+                  currentIndex: _selectedIndex,
+                  selectedItemColor: isDark ? kAccentColor : kBlueNavyColor,
+                  items: [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.account_circle),
+                      label: 'Account',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.alternate_email_sharp),
+                      label: 'Aliases',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.more_horiz_rounded),
+                      label: 'More',
+                    ),
+                  ],
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.more_horiz_rounded),
-              label: 'More',
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
 
-  Widget buildAppBar(BuildContext context) {
+  Widget buildAppBar(BuildContext context, bool isOffline) {
     return AppBar(
       elevation: 0,
       title: Text('AddyManager', style: TextStyle(color: Colors.white)),
       centerTitle: true,
       leading: IconButton(
         icon: Icon(Icons.add_circle_outline_outlined),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-            ),
-            builder: (context) {
-              return SingleChildScrollView(
-                padding:
-                    EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 20),
-                child: CreateNewAlias(),
-              );
-            },
-          );
-        },
+        onPressed: isOffline
+            ? () => Fluttertoast.showToast(
+                  msg: 'Can not create alias while offline',
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  backgroundColor: Colors.grey[600],
+                )
+            : () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  builder: (context) {
+                    return SingleChildScrollView(
+                      padding: EdgeInsets.only(
+                          left: 20, right: 20, top: 0, bottom: 20),
+                      child: CreateNewAlias(),
+                    );
+                  },
+                );
+              },
       ),
       actions: [
         IconButton(
