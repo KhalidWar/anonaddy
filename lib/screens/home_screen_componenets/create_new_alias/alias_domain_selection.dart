41f7eca1a276e6df24d9c216e05a6ad98c2ba8fb
Khalid War <git@khalidwar.com>
2021-09-08 21:39:19 -0500

    Updated select alias domain UI on create new alias
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/home_screen_componenets/create_new_alias/alias_domain_selection.dart
+++ lib/screens/home_screen_componenets/create_new_alias/alias_domain_selection.dart
@@ -1,5 +1,6 @@
 import 'package:anonaddy/models/domain_options/domain_options.dart';
 import 'package:anonaddy/shared_components/bottom_sheet_header.dart';
+import 'package:anonaddy/shared_components/constants/material_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,58 +13,71 @@ class AliasDomainSelection extends StatelessWidget {
   final List<String> aliasFormatList;
   final DomainOptions domainOptions;
 
+  void setAliasDomain(BuildContext context, String selectedDomain) {
+    final aliasStateProvider = context.read(aliasStateManagerProvider);
+    aliasStateProvider.setAliasDomain = selectedDomain;
+
+    if (aliasStateProvider.sharedDomains.contains(selectedDomain)) {
+      aliasStateProvider.setAliasFormat = aliasFormatList[0];
+    } else {
+      aliasStateProvider.setAliasFormat = domainOptions.defaultAliasFormat;
+    }
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final aliasStateProvider = context.read(aliasStateManagerProvider);
+    final size = MediaQuery.of(context).size;
 
-    final isDark = Theme.of(context).brightness == Brightness.dark;
-
-    int aliasDomainIndex = 0;
-
-    void setAliasDomain() {
-      final selectedDomain = domainOptions.domains[aliasDomainIndex];
-      aliasStateProvider.setAliasDomain = selectedDomain;
-      if (aliasStateProvider.sharedDomains.contains(selectedDomain)) {
-        aliasStateProvider.setAliasFormat = aliasFormatList[0];
-      } else {
-        aliasStateProvider.setAliasFormat = domainOptions.defaultAliasFormat;
-      }
-      Navigator.pop(context);
-    }
-
-    return Column(
-      children: [
-        BottomSheetHeader(headerLabel: 'Select Alias Domain'),
-        Expanded(
-          child: CupertinoPicker(
-            itemExtent: 50,
-            diameterRatio: 10,
-            squeeze: 1,
-            selectionOverlay: Container(),
-            backgroundColor: Colors.transparent,
-            onSelectedItemChanged: (index) {
-              aliasDomainIndex = index;
-            },
-            children: domainOptions.domains.map<Widget>((value) {
-              return Text(
-                value,
-                style: TextStyle(
-                  color: isDark ? Colors.white : null,
-                ),
-              );
-            }).toList(),
-          ),
-        ),
-        Container(
-          width: double.infinity,
-          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(),
-            child: Text('Done'),
-            onPressed: () => setAliasDomain(),
-          ),
-        ),
-      ],
+    return DraggableScrollableSheet(
+      expand: false,
+      initialChildSize: 0.5,
+      minChildSize: 0.3,
+      maxChildSize: 0.7,
+      builder: (context, controller) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BottomSheetHeader(headerLabel: 'Select Alias Domain'),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                controller: controller,
+                children: [
+                  if (domainOptions.domains.isEmpty)
+                    Center(
+                      child: Text(
+                        'No alias domain found',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    )
+                  else
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: domainOptions.domains.length,
+                      itemBuilder: (context, index) {
+                        final domain = domainOptions.domains[index];
+                        return ListTile(
+                          dense: true,
+                          selectedTileColor: kAccentColor,
+                          horizontalTitleGap: 0,
+                          title: Text(
+                            domain,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                          onTap: () => setAliasDomain(context, domain),
+                        );
+                      },
+                    ),
+                ],
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+          ],
+        );
+      },
     );
   }
 }
