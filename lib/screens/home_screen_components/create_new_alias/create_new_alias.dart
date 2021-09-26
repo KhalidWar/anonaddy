2a50f510bf22515e4992d8acbdc004aeb5ef3e82
Khalid War <git@khalidwar.com>
2021-09-25 23:59:40 -0500

    Refactored createAlias code
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/home_screen_components/create_new_alias/create_new_alias.dart
+++ lib/screens/home_screen_components/create_new_alias/create_new_alias.dart
@@ -1,6 +1,5 @@
 import 'package:anonaddy/global_providers.dart';
 import 'package:anonaddy/models/account/account_model.dart';
-import 'package:anonaddy/models/domain_options/domain_options.dart';
 import 'package:anonaddy/shared_components/bottom_sheet_header.dart';
 import 'package:anonaddy/shared_components/constants/material_constants.dart';
 import 'package:anonaddy/shared_components/constants/official_anonaddy_strings.dart';
@@ -30,22 +29,18 @@ class _CreateNewAliasState extends State<CreateNewAlias> {
   String _description = '';
   String _localPart = '';
 
+  String? aliasDomain;
+  String? aliasFormat;
+
+  String? defaultAliasDomain;
+  String? defaultAliasFormat;
+
   bool isAliasDomainError = false;
   bool isAliasFormatError = false;
-
-  Future<void> createNewAlias(String aliasDomain, aliasFormat) async {
-    await context
-        .read(aliasStateManagerProvider)
-        .createNewAlias(_description, aliasDomain, aliasFormat, _localPart);
-    Navigator.pop(context);
-  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
-    final createAliasText =
-        'Other aliases e.g. alias@${widget.account.username}.anonaddy.com or .me can also be created automatically when they receive their first email.';
 
     return Consumer(
       builder: (context, watch, _) {
@@ -59,169 +54,12 @@ class _CreateNewAliasState extends State<CreateNewAlias> {
             );
 
           case DomainOptionsStatus.loaded:
-            final domainOptions = domainOptionsState.domainOptions!;
-            final correctAliasString =
-                context.read(nicheMethods).correctAliasString;
+            defaultAliasDomain =
+                domainOptionsState.domainOptions!.defaultAliasDomain;
+            defaultAliasFormat =
+                domainOptionsState.domainOptions!.defaultAliasFormat;
 
-            final aliasStateProvider = watch(aliasStateManagerProvider);
-            final isLoading = aliasStateProvider.isToggleLoading;
-            String? aliasDomain = aliasStateProvider.aliasDomain;
-            String? aliasFormat = aliasStateProvider.aliasFormat;
-
-            List<String> getAliasFormatList() {
-              if (aliasStateProvider.sharedDomains.contains(aliasDomain)) {
-                if (widget.account.subscription == kFreeSubscription) {
-                  return aliasStateProvider.freeTierWithSharedDomain;
-                } else {
-                  return aliasStateProvider.paidTierWithSharedDomain;
-                }
-              } else {
-                if (widget.account.subscription == kFreeSubscription) {
-                  return aliasStateProvider.freeTierNoSharedDomain;
-                } else {
-                  return aliasStateProvider.paidTierNoSharedDomain;
-                }
-              }
-            }
-
-            //todo Clean up this mess!!
-            Future<void> createAlias(DomainOptions domainOptions) async {
-              final isDomainNull = aliasDomain == null &&
-                  domainOptions.defaultAliasDomain == null;
-              final isFormatNull = aliasFormat == null &&
-                  domainOptions.defaultAliasFormat == null;
-
-              if (!isDomainNull) {
-                if (!isFormatNull) {
-                  if (aliasFormat == kCustom) {
-                    if (_localPartFormKey.currentState!.validate()) {
-                      await createNewAlias(
-                        aliasDomain ?? domainOptions.defaultAliasDomain!,
-                        aliasFormat ?? domainOptions.defaultAliasFormat!,
-                      );
-                    }
-                  } else {
-                    await createNewAlias(
-                      aliasDomain ?? domainOptions.defaultAliasDomain!,
-                      aliasFormat ?? domainOptions.defaultAliasFormat!,
-                    );
-                  }
-                } else {
-                  setState(() {
-                    isAliasFormatError = true;
-                  });
-                }
-              } else {
-                setState(() {
-                  isAliasDomainError = true;
-                });
-              }
-            }
-
-            if (aliasDomain == null)
-              aliasDomain = domainOptions.defaultAliasDomain;
-            if (aliasFormat == null)
-              aliasFormat = domainOptions.defaultAliasFormat;
-
-            return Container(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    BottomSheetHeader(headerLabel: kCreateNewAlias),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: 15, right: 15, top: 0, bottom: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(createAliasText),
-                          SizedBox(height: size.height * 0.01),
-                          TextFormField(
-                            textInputAction: TextInputAction.next,
-                            onChanged: (input) => _description = input,
-                            decoration: kTextFormFieldDecoration.copyWith(
-                              hintText: kDescriptionFieldHint,
-                              contentPadding:
-                                  EdgeInsets.symmetric(horizontal: 10),
-                            ),
-                          ),
-                          if (aliasFormat == kCustom)
-                            Column(
-                              children: [
-                                SizedBox(height: size.height * 0.01),
-                                Form(
-                                  key: _localPartFormKey,
-                                  child: TextFormField(
-                                    onChanged: (input) => _localPart = input,
-                                    validator: (input) => context
-                                        .read(formValidator)
-                                        .validateLocalPart(input!),
-                                    textInputAction: TextInputAction.next,
-                                    decoration:
-                                        kTextFormFieldDecoration.copyWith(
-                                      hintText: kLocalPartFieldHint,
-                                      contentPadding:
-                                          EdgeInsets.symmetric(horizontal: 10),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          SizedBox(height: size.height * 0.01),
-                          aliasDomainFormatDropdown(
-                            context: context,
-                            title: 'Alias Domain',
-                            label: aliasDomain ?? kSelectAliasDomain,
-                            isError: isAliasDomainError,
-                            child: AliasDomainSelection(
-                              aliasFormatList: getAliasFormatList(),
-                              domainOptions: domainOptions,
-                            ),
-                          ),
-                          SizedBox(height: size.height * 0.01),
-                          aliasDomainFormatDropdown(
-                            context: context,
-                            title: 'Alias Format',
-                            label: aliasFormat == null
-                                ? kSelectAliasFormat
-                                : correctAliasString(aliasFormat),
-                            isError: isAliasFormatError,
-                            child: AliasFormatSelection(
-                                aliasFormatList: getAliasFormatList()),
-                          ),
-                          if (aliasFormat == kCustom)
-                            Text(
-                              kCreateAliasCustomFieldNote,
-                              style: Theme.of(context).textTheme.caption,
-                            ),
-                          SizedBox(height: size.height * 0.02),
-                          recipientsDropdown(context),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(),
-                        child: isLoading
-                            ? context
-                                .read(customLoadingIndicator)
-                                .customLoadingIndicator()
-                            : Text(kCreateAlias),
-                        onPressed: isLoading
-                            ? () {}
-                            : () => createAlias(domainOptions),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return crateAliasWidget(domainOptionsState);
 
           case DomainOptionsStatus.failed:
             final error = domainOptionsState.errorMessage;
@@ -231,6 +69,179 @@ class _CreateNewAliasState extends State<CreateNewAlias> {
               lottieHeight: MediaQuery.of(context).size.height * 0.2,
             );
         }
+      },
+    );
+  }
+
+  Widget crateAliasWidget(DomainOptionsState domainOptionsState) {
+    final size = MediaQuery.of(context).size;
+
+    final createAliasText =
+        'Other aliases e.g. alias@${widget.account.username}.anonaddy.com or .me can also be created automatically when they receive their first email.';
+
+    Future<void> createNewAlias(String aliasDomain, aliasFormat) async {
+      await context
+          .read(aliasStateManagerProvider)
+          .createNewAlias(_description, aliasDomain, aliasFormat, _localPart);
+      Navigator.pop(context);
+    }
+
+    //todo Clean up this mess!!
+    Future<void> createAlias() async {
+      final isDomainNull = aliasDomain == null &&
+          domainOptionsState.domainOptions!.defaultAliasDomain == null;
+      final isFormatNull = aliasFormat == null &&
+          domainOptionsState.domainOptions!.defaultAliasFormat == null;
+
+      if (!isDomainNull) {
+        if (!isFormatNull) {
+          if (aliasFormat == kCustom) {
+            if (_localPartFormKey.currentState!.validate()) {
+              await createNewAlias(
+                aliasDomain ?? defaultAliasDomain!,
+                aliasFormat ?? defaultAliasFormat!,
+              );
+            }
+          } else {
+            await createNewAlias(
+              aliasDomain ?? defaultAliasDomain!,
+              aliasFormat ?? defaultAliasFormat!,
+            );
+          }
+        } else {
+          setState(() {
+            isAliasFormatError = true;
+          });
+        }
+      } else {
+        setState(() {
+          isAliasDomainError = true;
+        });
+      }
+    }
+
+    return Consumer(
+      builder: (context, watch, _) {
+        final aliasStateProvider = watch(aliasStateManagerProvider);
+        final isLoading = aliasStateProvider.isToggleLoading;
+        aliasDomain = aliasStateProvider.aliasDomain;
+        aliasFormat = aliasStateProvider.aliasFormat;
+
+        List<String> getAliasFormatList() {
+          if (aliasStateProvider.sharedDomains.contains(aliasDomain)) {
+            if (widget.account.subscription == kFreeSubscription) {
+              return aliasStateProvider.freeTierWithSharedDomain;
+            } else {
+              return aliasStateProvider.paidTierWithSharedDomain;
+            }
+          } else {
+            if (widget.account.subscription == kFreeSubscription) {
+              return aliasStateProvider.freeTierNoSharedDomain;
+            } else {
+              return aliasStateProvider.paidTierNoSharedDomain;
+            }
+          }
+        }
+
+        if (aliasDomain == null) aliasDomain = defaultAliasDomain;
+        if (aliasFormat == null) aliasFormat = defaultAliasFormat;
+
+        return Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                BottomSheetHeader(headerLabel: kCreateNewAlias),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(createAliasText),
+                      SizedBox(height: size.height * 0.01),
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        onChanged: (input) => _description = input,
+                        decoration: kTextFormFieldDecoration.copyWith(
+                          hintText: kDescriptionFieldHint,
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                        ),
+                      ),
+                      if (aliasFormat == kCustom)
+                        Column(
+                          children: [
+                            SizedBox(height: size.height * 0.01),
+                            Form(
+                              key: _localPartFormKey,
+                              child: TextFormField(
+                                onChanged: (input) => _localPart = input,
+                                validator: (input) => context
+                                    .read(formValidator)
+                                    .validateLocalPart(input!),
+                                textInputAction: TextInputAction.next,
+                                decoration: kTextFormFieldDecoration.copyWith(
+                                  hintText: kLocalPartFieldHint,
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 10),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      SizedBox(height: size.height * 0.01),
+                      aliasDomainFormatDropdown(
+                        context: context,
+                        title: 'Alias Domain',
+                        label: aliasDomain ?? kSelectAliasDomain,
+                        isError: isAliasDomainError,
+                        child: AliasDomainSelection(
+                          aliasFormatList: getAliasFormatList(),
+                          domainOptions: domainOptionsState.domainOptions!,
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.01),
+                      aliasDomainFormatDropdown(
+                        context: context,
+                        title: 'Alias Format',
+                        label: aliasFormat == null
+                            ? kSelectAliasFormat
+                            : context
+                                .read(nicheMethods)
+                                .correctAliasString(aliasFormat!),
+                        isError: isAliasFormatError,
+                        child: AliasFormatSelection(
+                            aliasFormatList: getAliasFormatList()),
+                      ),
+                      if (aliasFormat == kCustom)
+                        Text(
+                          kCreateAliasCustomFieldNote,
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      SizedBox(height: size.height * 0.02),
+                      recipientsDropdown(context),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    child: isLoading
+                        ? context
+                            .read(customLoadingIndicator)
+                            .customLoadingIndicator()
+                        : Text(kCreateAlias),
+                    onPressed: isLoading ? () {} : () => createAlias(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
