8f6ad7e956af5fd02c3fe19b68d453bdb58323e1
Khalid War <git@khalidwar.com>
2021-09-25 20:05:35 -0500

    Overhauled how Domains and its state are accessed
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/account_tab/domains/domains.dart
+++ lib/screens/account_tab/domains/domains.dart
@@ -1,4 +1,3 @@
-import 'package:anonaddy/global_providers.dart';
 import 'package:anonaddy/models/domain/domain_model.dart';
 import 'package:anonaddy/screens/account_tab/components/paid_feature_wall.dart';
 import 'package:anonaddy/shared_components/constants/official_anonaddy_strings.dart';
@@ -8,6 +7,8 @@ import 'package:anonaddy/shared_components/lottie_widget.dart';
 import 'package:anonaddy/shared_components/shimmer_effects/recipients_shimmer_loading.dart';
 import 'package:anonaddy/state_management/account/account_notifier.dart';
 import 'package:anonaddy/state_management/account/account_state.dart';
+import 'package:anonaddy/state_management/domains/domains_notifier.dart';
+import 'package:anonaddy/state_management/domains/domains_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -28,29 +29,33 @@ class Domains extends ConsumerWidget {
           return PaidFeatureWall();
         }
 
-        final domainData = watch(domainsProvider);
-        return domainData.when(
-          loading: () => RecipientsShimmerLoading(),
-          data: (domainModel) {
-            if (domainModel.domains.isEmpty) {
+        final domainsState = watch(domainsStateNotifier);
+
+        switch (domainsState.status) {
+          case DomainsStatus.loading:
+            return RecipientsShimmerLoading();
+
+          case DomainsStatus.loaded:
+            final domains = domainsState.domainModel!.domains;
+            if (domains.isEmpty) {
               return Center(
                 child: Text(
                   'No domains found',
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
               );
-            } else {
-              return domainsList(domainModel);
             }
-          },
-          error: ((error, stackTrace) {
+
+            return domainsList(domains);
+
+          case DomainsStatus.failed:
+            final error = domainsState.errorMessage;
             return LottieWidget(
               lottie: 'assets/lottie/errorCone.json',
               lottieHeight: MediaQuery.of(context).size.height * 0.1,
               label: error.toString(),
             );
-          }),
-        );
+        }
 
       case AccountStatus.failed:
         return LottieWidget(
@@ -61,12 +66,12 @@ class Domains extends ConsumerWidget {
     }
   }
 
-  ListView domainsList(DomainModel domainModel) {
+  ListView domainsList(List<Domain> domains) {
     return ListView.builder(
       shrinkWrap: true,
-      itemCount: domainModel.domains.length,
+      itemCount: domains.length,
       itemBuilder: (context, index) {
-        final domain = domainModel.domains[index];
+        final domain = domains[index];
 
         return InkWell(
           child: Padding(
