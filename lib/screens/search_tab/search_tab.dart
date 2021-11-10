68bc0e0ab0dc7d8af84e851155e62a7ae7619a51
Khalid War <git@khalidwar.com>
2021-11-09 20:22:03 -0600

    Extracted SearchHistory to its own widget
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/search_tab/search_tab.dart
+++ lib/screens/search_tab/search_tab.dart
@@ -1,14 +1,11 @@
 import 'package:anonaddy/global_providers.dart';
+import 'package:anonaddy/screens/search_tab/components/search_history.dart';
 import 'package:anonaddy/services/search/search_service.dart';
 import 'package:anonaddy/shared_components/constants/material_constants.dart';
 import 'package:anonaddy/shared_components/constants/ui_strings.dart';
-import 'package:anonaddy/shared_components/list_tiles/alias_list_tile.dart';
-import 'package:anonaddy/shared_components/platform_aware_widgets/platform_scroll_bar.dart';
 import 'package:anonaddy/state_management/alias_state/alias_tab_notifier.dart';
 import 'package:anonaddy/state_management/alias_state/alias_tab_state.dart';
-import 'package:anonaddy/state_management/alias_state/fab_visibility_state.dart';
 import 'package:anonaddy/state_management/search/search_history_notifier.dart';
-import 'package:anonaddy/state_management/search/search_history_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -71,61 +68,8 @@ class SearchTab extends StatelessWidget {
           ),
         ),
         Divider(height: 0),
-        searchHistory(context),
+        const SearchHistory(),
       ],
-    );
-  }
-
-  Widget searchHistory(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return Consumer(
-      builder: (context, watch, _) {
-        final searchState = watch(searchHistoryStateNotifier);
-
-        switch (searchState.status) {
-          case SearchHistoryStatus.loading:
-            return Padding(
-              padding: EdgeInsets.all(size.height * 0.01),
-              child: Center(child: CircularProgressIndicator()),
-            );
-
-          case SearchHistoryStatus.loaded:
-            final aliases = searchState.aliases;
-
-            if (aliases.isEmpty)
-              return Padding(
-                padding: EdgeInsets.all(size.height * 0.01),
-                child: Row(children: [Text('Nothing to see here.')]),
-              );
-            else
-              return Expanded(
-                child: PlatformScrollbar(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: aliases.length,
-                    controller: context
-                        .read(fabVisibilityStateNotifier.notifier)
-                        .searchController,
-                    itemBuilder: (context, index) {
-                      return AliasListTile(aliasData: aliases[index]);
-                    },
-                  ),
-                ),
-              );
-
-          case SearchHistoryStatus.failed:
-            final error = searchState.errorMessage;
-            return Padding(
-              padding: EdgeInsets.all(size.height * 0.01),
-              child: Row(
-                children: [
-                  Text(error ?? 'Something went wrong: ${error.toString()}'),
-                ],
-              ),
-            );
-        }
-      },
     );
   }
 }
