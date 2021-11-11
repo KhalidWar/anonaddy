9e5d95e26988859283aa46cc767ad8dae228d245
Khalid War <git@khalidwar.com>
2021-11-10 20:16:59 -0600

    Reduced iOS native protection screen time by half
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/authorization_screen/authorization_screen.dart
+++ lib/screens/authorization_screen/authorization_screen.dart
@@ -28,7 +28,7 @@ class AuthorizationScreen extends StatelessWidget {
 
             /// Disables screenshot and blocks app view on Recent Apps
             return SecureApplication(
-              nativeRemoveDelay: 1000,
+              nativeRemoveDelay: 500,
               child: Builder(
                 builder: (context) {
                   /// Access secureApp provider to control its state
