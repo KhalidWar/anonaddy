9c5d8b45cb2bc3c5037278066e2e526108b1934f
Khalid War <git@khalidwar.com>
2021-12-11 13:22:08 -0600

    Fixed Alias Shimmer display issue
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/alias_tab/components/alias_shimmer_loading.dart
+++ lib/screens/alias_tab/components/alias_shimmer_loading.dart
@@ -9,131 +9,66 @@ class AliasShimmerLoading extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final isDark = Theme.of(context).brightness == Brightness.dark;
 
-    return TabBarView(
-      children: [
-        Shimmer.fromColors(
-          baseColor: isDark ? Colors.grey : Colors.grey[400]!,
-          highlightColor: Colors.grey[100]!,
-          enabled: true,
-          child: ListView.builder(
-            padding: EdgeInsets.all(0),
-            itemCount: 12,
-            itemBuilder: (_, __) {
-              return ListTile(
-                dense: true,
-                horizontalTitleGap: 0,
-                contentPadding:
-                    EdgeInsets.only(left: 6, right: 18, top: 0, bottom: 0),
-                leading: Container(
-                  width: 30.0,
-                  height: 30.0,
-                  decoration: BoxDecoration(
+    return Shimmer.fromColors(
+      baseColor: isDark ? Colors.grey : Colors.grey[400]!,
+      highlightColor: Colors.grey[100]!,
+      enabled: true,
+      child: ListView.builder(
+        padding: EdgeInsets.all(0),
+        itemCount: 12,
+        itemBuilder: (_, __) {
+          return ListTile(
+            dense: true,
+            horizontalTitleGap: 0,
+            contentPadding:
+                EdgeInsets.only(left: 6, right: 18, top: 0, bottom: 0),
+            leading: Container(
+              width: 30.0,
+              height: 30.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40),
+              ),
+            ),
+            title: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 50,
+                    height: 10.0,
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(40),
                   ),
                 ),
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: 50,
-                        height: 10.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: size.width * 0.1),
-                    ),
-                  ],
+                Padding(
+                  padding: EdgeInsets.only(left: size.width * 0.1),
                 ),
-                subtitle: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 10.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: size.width * 0.45),
-                    ),
-                  ],
-                ),
-                trailing: Container(
-                  width: 20.0,
-                  height: 25.0,
-                  margin: EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(
+              ],
+            ),
+            subtitle: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 10.0,
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-        Shimmer.fromColors(
-          baseColor: isDark ? Colors.grey : Colors.grey[300]!,
-          highlightColor: Colors.grey[100]!,
-          enabled: true,
-          child: ListView.builder(
-            padding: EdgeInsets.all(0),
-            itemCount: 12,
-            itemBuilder: (_, __) {
-              return ListTile(
-                dense: true,
-                horizontalTitleGap: 0,
-                contentPadding:
-                    EdgeInsets.only(left: 6, right: 18, top: 0, bottom: 0),
-                leading: Container(
-                  width: 30.0,
-                  height: 30.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40),
-                  ),
+                Padding(
+                  padding: EdgeInsets.only(left: size.width * 0.45),
                 ),
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: 50,
-                        height: 10.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: size.width * 0.1),
-                    ),
-                  ],
-                ),
-                subtitle: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 10.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: size.width * 0.45),
-                    ),
-                  ],
-                ),
-                trailing: Container(
-                  width: 20.0,
-                  height: 25.0,
-                  margin: EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-      ],
+              ],
+            ),
+            trailing: Container(
+              width: 20.0,
+              height: 25.0,
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
