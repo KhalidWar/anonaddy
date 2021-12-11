2dcffd3ef613574906467e71a79b8be92377b5fb
Khalid War <git@khalidwar.com>
2021-12-11 00:30:25 -0600

    AliasTabStat widgets now gets data from AccountData endpoint
    Signed-off-by: Khalid War <git@khalidwar.com>

--- lib/screens/alias_tab/alias_tab.dart
+++ lib/screens/alias_tab/alias_tab.dart
@@ -12,132 +12,93 @@ import 'components/alias_shimmer_loading.dart';
 import 'components/alias_tab_pie_chart.dart';
 import 'components/empty_list_alias_tab.dart';
 
-class AliasTab extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final aliasTabState = watch(aliasTabStateNotifier);
-
-    switch (aliasTabState.status) {
-      case AliasTabStatus.loading:
-        return const AliasShimmerLoading();
-
-      case AliasTabStatus.loaded:
-        return const LoadedAliasTab();
-
-      case AliasTabStatus.failed:
-        final error = aliasTabState.errorMessage!;
-        return LottieWidget(
-          showLoading: true,
-          lottie: 'assets/lottie/errorCone.json',
-          label: error.toString(),
-        );
-    }
-  }
-}
-
-class LoadedAliasTab extends StatefulWidget {
-  const LoadedAliasTab({Key? key}) : super(key: key);
-
-  @override
-  _LoadedAliasTabState createState() => _LoadedAliasTabState();
-}
-
-class _LoadedAliasTabState extends State<LoadedAliasTab> {
+class AliasTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Consumer(
-      builder: (context, watch, _) {
-        final aliasTabState = watch(aliasTabStateNotifier);
-        final availableAliasList = aliasTabState.availableAliasList;
-        final deletedAliasList = aliasTabState.deletedAliasList;
-
-        return Scaffold(
-          body: DefaultTabController(
-            length: 2,
-            child: NestedScrollView(
-              controller: context
-                  .read(fabVisibilityStateNotifier.notifier)
-                  .aliasController,
-              headerSliverBuilder: (context, innerBoxIsScrolled) {
-                return [
-                  SliverAppBar(
-                    expandedHeight: size.height * 0.25,
-                    elevation: 0,
-                    floating: true,
-                    pinned: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                      collapseMode: CollapseMode.pin,
-                      background: AliasTabPieChart(
-                        emailsForwarded: aliasTabState.forwardedList,
-                        emailsBlocked: aliasTabState.blockedList,
-                        emailsReplied: aliasTabState.repliedList,
-                        emailsSent: aliasTabState.sentList,
-                      ),
-                    ),
-                    bottom: TabBar(
-                      indicatorColor: kAccentColor,
-                      tabs: [
-                        Tab(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const Text('Available Aliases'),
-                              Text('${availableAliasList.length}'),
-                            ],
-                          ),
-                        ),
-                        Tab(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const Text('Deleted Aliases'),
-                              Text('${deletedAliasList.length}'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ];
-              },
-              body: TabBarView(
-                children: [
-                  if (availableAliasList.isEmpty)
-                    const EmptyListAliasTabWidget()
-                  else
-                    PlatformScrollbar(
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: availableAliasList.length,
-                        itemBuilder: (context, index) {
-                          return AliasListTile(
-                            aliasData: availableAliasList[index],
-                          );
-                        },
-                      ),
-                    ),
-                  if (deletedAliasList.isEmpty)
-                    const EmptyListAliasTabWidget()
-                  else
-                    PlatformScrollbar(
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: deletedAliasList.length,
-                        itemBuilder: (context, index) {
-                          return AliasListTile(
-                            aliasData: deletedAliasList[index],
-                          );
-                        },
-                      ),
-                    ),
-                ],
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          controller:
+              context.read(fabVisibilityStateNotifier.notifier).aliasController,
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                expandedHeight: size.height * 0.25,
+                elevation: 0,
+                floating: true,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  background: const AliasTabPieChart(),
+                ),
+                bottom: TabBar(
+                  indicatorColor: kAccentColor,
+                  tabs: [
+                    Tab(child: const Text('Available Aliases')),
+                    Tab(child: const Text('Deleted Aliases')),
+                  ],
+                ),
               ),
-            ),
+            ];
+          },
+          body: Consumer(
+            builder: (context, watch, _) {
+              final aliasTabState = watch(aliasTabStateNotifier);
+
+              switch (aliasTabState.status) {
+                case AliasTabStatus.loading:
+                  return const AliasShimmerLoading();
+
+                case AliasTabStatus.loaded:
+                  final availableAliasList = aliasTabState.availableAliasList;
+                  final deletedAliasList = aliasTabState.deletedAliasList;
+                  return TabBarView(
+                    children: [
+                      if (availableAliasList.isEmpty)
+                        const EmptyListAliasTabWidget()
+                      else
+                        PlatformScrollbar(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: availableAliasList.length,
+                            itemBuilder: (context, index) {
+                              return AliasListTile(
+                                aliasData: availableAliasList[index],
+                              );
+                            },
+                          ),
+                        ),
+                      if (deletedAliasList.isEmpty)
+                        const EmptyListAliasTabWidget()
+                      else
+                        PlatformScrollbar(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: deletedAliasList.length,
+                            itemBuilder: (context, index) {
+                              return AliasListTile(
+                                aliasData: deletedAliasList[index],
+                              );
+                            },
+                          ),
+                        ),
+                    ],
+                  );
+
+                case AliasTabStatus.failed:
+                  final error = aliasTabState.errorMessage!;
+                  return LottieWidget(
+                    lottie: 'assets/lottie/errorCone.json',
+                    label: error.toString(),
+                  );
+              }
+            },
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/alias_tab/components/aliases_stats_shimmer.dart
@@ -0,0 +1,71 @@
+import 'package:anonaddy/shared_components/constants/material_constants.dart';
+import 'package:anonaddy/shared_components/pie_chart/pie_chart_indicator.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class AliasesStatsShimmer extends StatelessWidget {
+  const AliasesStatsShimmer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final pieChartSectionRadius = 50.0;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            PieChartIndicator(
+              color: kFirstPieChartColor,
+              label: 'emails forwarded',
+              count: 0,
+              textColor: Colors.white,
+            ),
+            SizedBox(height: size.height * 0.02),
+            PieChartIndicator(
+              color: kSecondPieChartColor,
+              label: 'emails blocked',
+              count: 0,
+              textColor: Colors.white,
+            ),
+            SizedBox(height: size.height * 0.02),
+            PieChartIndicator(
+              color: kFourthPieChartColor,
+              label: 'emails replied',
+              count: 0,
+              textColor: Colors.white,
+            ),
+            SizedBox(height: size.height * 0.02),
+            PieChartIndicator(
+              color: kThirdPieChartColor,
+              label: 'emails sent',
+              count: 0,
+              textColor: Colors.white,
+            ),
+          ],
+        ),
+        Container(
+          height: size.height * 0.18,
+          width: size.height * 0.18,
+          child: PieChart(
+            PieChartData(
+              borderData: FlBorderData(show: false),
+              sectionsSpace: 0,
+              sections: [
+                PieChartSectionData(
+                  showTitle: false,
+                  radius: pieChartSectionRadius,
+                  color: Colors.white54,
+                  value: 1,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
