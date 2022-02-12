0d733a245e26f474cfc009dec8c7c585c1dabc03
Khalid Warsame <dev@khalidwar.com>
2022-02-12 10:40:30 -0600

    Upgraded to Flutter 2.10.1 and fixed Android files breaking changes
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- android/app/src/main/kotlin/com/khalidwar/anonaddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/khalidwar/anonaddy/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.khalidwar.anonaddy
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-}
+class MainActivity: FlutterActivity() {}
