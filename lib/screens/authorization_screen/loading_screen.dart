5242bbcdd8f983613cbe589b388f93032da0ce87
Khalid Warsame <dev@khalidwar.com>
2022-02-20 19:12:47 -0600

    Added logout option to loading if it persists for 10 seconds
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/screens/authorization_screen/loading_screen.dart
+++ lib/screens/authorization_screen/loading_screen.dart
@@ -1,8 +1,42 @@
-import 'package:anonaddy/shared_components/constants/material_constants.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class LoadingScreen extends StatelessWidget {
+import 'package:anonaddy/shared_components/constants/material_constants.dart';
+import 'package:anonaddy/shared_components/constants/ui_strings.dart';
+import 'package:anonaddy/shared_components/platform_aware_widgets/dialogs/platform_alert_dialog.dart';
+import 'package:anonaddy/shared_components/platform_aware_widgets/platform_aware.dart';
+import 'package:anonaddy/shared_components/platform_aware_widgets/platform_button.dart';
+import 'package:anonaddy/shared_components/platform_aware_widgets/platform_loading_indicator.dart';
+import 'package:anonaddy/state_management/authorization/auth_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LoadingScreen extends ConsumerStatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends ConsumerState<LoadingScreen> {
+  static const errorNote =
+      'Process is taking longer than usual.\nIf it persists, please log out and login back in.';
+  bool showLogoutButton = false;
+
+  Future<void> showLogoutOption() async {
+    await Future.delayed(const Duration(seconds: 10), () async {
+      setState(() {
+        showLogoutButton = true;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      showLogoutOption();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +56,45 @@ class LoadingScreen extends StatelessWidget {
           Container(
             alignment: Alignment.bottomCenter,
             padding: const EdgeInsets.all(20),
-            child: const CircularProgressIndicator(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: showLogoutButton
+                  ? [
+                      const PlatformLoadingIndicator(),
+                      const SizedBox(height: 10),
+                      Text(
+                        errorNote,
+                        style: Theme.of(context).textTheme.caption,
+                        textAlign: TextAlign.center,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.only(top: 10),
+                        child: PlatformButton(
+                          color: Colors.red,
+                          child: const Text('Logout'),
+                          onPress: () => logout(),
+                        ),
+                      ),
+                    ]
+                  : [const PlatformLoadingIndicator()],
+            ),
           ),
         ],
+      ),
+    );
+  }
+
+  void logout() {
+    /// Show platform dialog
+    PlatformAware.platformDialog(
+      context: context,
+      child: PlatformAlertDialog(
+        title: 'Logout',
+        content: kLogOutAlertDialog,
+        method: () async {
+          await ref.read(authStateNotifier.notifier).logout(context);
+        },
       ),
     );
   }
