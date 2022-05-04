00d6c35fcb81a5b4c7feeaabac8794b84d41eb69
Khalid Warsame <dev@khalidwar.com>
2022-05-03 20:31:04 -0500

    Added AccountTab widget keys
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

---
+++ lib/screens/account_tab/components/account_tab_widget_keys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AccountTabWidgetKeys {
+  /// Account Tab
+  static const accountTabScaffold = Key('accountTabScaffold');
+  static const accountTabSliverAppBar = Key('accountTabSliverAppBar');
+  static const accountTabHeader = Key('accountTabHeader');
+}
