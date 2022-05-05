e33b30f0e89ccdd2dfe777a88596813e01e26005
Khalid Warsame <dev@khalidwar.com>
2022-05-05 18:14:53 -0500

    Fixed AccountPopupInfo display errors
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/screens/account_tab/components/account_popup_info.dart
+++ lib/screens/account_tab/components/account_popup_info.dart
@@ -1,17 +1,45 @@
-import 'package:anonaddy/models/account/account.dart';
 import 'package:anonaddy/services/access_token/access_token_service.dart';
-import 'package:anonaddy/shared_components/constants/anonaddy_string.dart';
 import 'package:anonaddy/shared_components/constants/app_strings.dart';
+import 'package:anonaddy/state_management/account/account_state.dart';
 import 'package:anonaddy/utilities/niche_method.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AccountPopupInfo extends ConsumerWidget {
-  const AccountPopupInfo({Key? key, required this.account}) : super(key: key);
-  final Account account;
+  const AccountPopupInfo({
+    Key? key,
+    required this.accountState,
+  }) : super(key: key);
+  final AccountState accountState;
+
+  String getSubscriptionExpirationDate() {
+    /// Self hosted instances do NOT have a subscription and do not expire.
+    if (accountState.isSelfHosted) {
+      return AppStrings.subscriptionEndDateDoesNotExpire;
+    }
+
+    /// AnonAddy free subscriptions do NOT expire.
+    if (accountState.isSubscriptionFree) {
+      return AppStrings.subscriptionEndDateDoesNotExpire;
+    }
+
+    /// AnonAddy Lite and Pro subscriptions do expire.
+    return NicheMethod.fixDateTime(
+      accountState.account.subscriptionEndAt ??
+          AppStrings.subscriptionEndDateNotAvailable,
+    );
+  }
+
+  Future<void> updateDefaultAliasFormatDomain(WidgetRef ref) async {
+    final instanceURL =
+        await ref.read(accessTokenServiceProvider).getInstanceURL();
+    await NicheMethod.launchURL('https://$instanceURL/settings');
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final account = accountState.account;
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,36 +72,5 @@ class AccountPopupInfo extends ConsumerWidget {
         ),
       ],
     );
-  }
-
-  String getSubscriptionExpirationDate() {
-    final subscription = account.subscription;
-
-    /// Self hosted does NOT have a subscription and does not expire
-    if (subscription == null) {
-      return AppStrings.selfHosted;
-    }
-
-    /// Free subscription does NOT expire
-    if (subscription == AnonAddyString.subscriptionFree) {
-      return 'Does not expire';
-    }
-
-    /// Lite and Pro subscription expire
-    final liteSubscription = subscription == AnonAddyString.subscriptionLite;
-    final proSubscription = subscription == AnonAddyString.subscriptionPro;
-    if (liteSubscription || proSubscription) {
-      return NicheMethod.fixDateTime(
-        account.subscriptionEndAt ?? AppStrings.subscriptionEndDateNotAvailable,
-      );
-    }
-
-    return AppStrings.subscriptionEndDateNotAvailable;
-  }
-
-  Future<void> updateDefaultAliasFormatDomain(WidgetRef ref) async {
-    final instanceURL =
-        await ref.read(accessTokenServiceProvider).getInstanceURL();
-    await NicheMethod.launchURL('https://$instanceURL/settings');
   }
 }
