51cba65b78dd07692fc3877dbd8e865b434c3bc2
Khalid Warsame <dev@khalidwar.com>
2022-07-20 18:27:20 -0500

    Updated Username model
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/models/username/username.g.dart
+++ lib/models/username/username.g.dart
@@ -7,21 +7,22 @@ part of 'username.dart';
 // **************************************************************************
 
 Username _$UsernameFromJson(Map<String, dynamic> json) => Username(
-      id: json['id'] as String,
-      userId: json['user_id'] as String,
-      username: json['username'] as String,
-      description: json['description'] as String?,
+      id: json['id'] as String? ?? '',
+      userId: json['user_id'] as String? ?? '',
+      username: json['username'] as String? ?? '',
+      description: json['description'] as String? ?? '',
       aliases: (json['aliases'] as List<dynamic>?)
-          ?.map((e) => Alias.fromJson(e as Map<String, dynamic>))
-          .toList(),
+              ?.map((e) => Alias.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          const <Alias>[],
       defaultRecipient: json['default_recipient'] == null
           ? null
           : Recipient.fromJson(
               json['default_recipient'] as Map<String, dynamic>),
-      active: json['active'] as bool,
-      catchAll: json['catch_all'] as bool,
-      createdAt: DateTime.parse(json['created_at'] as String),
-      updatedAt: DateTime.parse(json['updated_at'] as String),
+      active: json['active'] as bool? ?? false,
+      catchAll: json['catch_all'] as bool? ?? false,
+      createdAt: json['created_at'] as String? ?? '',
+      updatedAt: json['updated_at'] as String? ?? '',
     );
 
 Map<String, dynamic> _$UsernameToJson(Username instance) => <String, dynamic>{
@@ -29,10 +30,10 @@ Map<String, dynamic> _$UsernameToJson(Username instance) => <String, dynamic>{
       'user_id': instance.userId,
       'username': instance.username,
       'description': instance.description,
-      'aliases': instance.aliases?.map((e) => e.toJson()).toList(),
+      'aliases': instance.aliases.map((e) => e.toJson()).toList(),
       'default_recipient': instance.defaultRecipient?.toJson(),
       'active': instance.active,
       'catch_all': instance.catchAll,
-      'created_at': instance.createdAt.toIso8601String(),
-      'updated_at': instance.updatedAt.toIso8601String(),
+      'created_at': instance.createdAt,
+      'updated_at': instance.updatedAt,
     };
