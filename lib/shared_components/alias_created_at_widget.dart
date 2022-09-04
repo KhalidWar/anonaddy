90791044d8002491c885e6e9c7e5c136efee56e4
Khalid Warsame <dev@khalidwar.com>
2022-09-04 11:22:01 -0500

    Updated AliasCreatedAtWidget UI
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/shared_components/alias_created_at_widget.dart
+++ lib/shared_components/alias_created_at_widget.dart
@@ -2,23 +2,35 @@ import 'package:anonaddy/utilities/niche_method.dart';
 import 'package:flutter/material.dart';
 
 class AliasCreatedAtWidget extends StatelessWidget {
-  const AliasCreatedAtWidget(
-      {Key? key, required this.label, required this.dateTime})
-      : super(key: key);
+  const AliasCreatedAtWidget({
+    Key? key,
+    required this.label,
+    required this.dateTime,
+  }) : super(key: key);
 
   final String label;
   final String dateTime;
 
   @override
   Widget build(BuildContext context) {
+    final locale = Localizations.localeOf(context);
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 12),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(label),
-          const SizedBox(width: 5),
           Text(
-            NicheMethod.convertStringToDateTime(dateTime),
+            NicheMethod.convertStringToDateTime(dateTime, locale),
+            style: Theme.of(context)
+                .textTheme
+                .caption
+                ?.copyWith(color: isDark ? Colors.white70 : Colors.black87),
+          ),
+          Text(
+            label,
+            style: Theme.of(context).textTheme.caption,
           ),
         ],
       ),
