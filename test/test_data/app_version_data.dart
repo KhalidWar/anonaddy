fcda5064de1bcbb1add063d893ec4da9241ebe48
Khalid Warsame <dev@khalidwar.com>
2023-10-25 17:28:28 -0500

    Fixed failing tests
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

---
+++ test/test_data/app_version_data.dart
@@ -0,0 +1,7 @@
+import 'package:anonaddy/models/app_version/app_version_model.dart';
+
+class AppVersionData {
+  static AppVersion validAppVersion() {
+    return AppVersion('1.0.0', 1, 0, 0);
+  }
+}
