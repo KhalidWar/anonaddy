5d90bdc4a4a22154b3f2ad7443346d3bbf6eb0ee
Khalid Warsame <dev@khalidwar.com>
2024-02-29 16:53:01 -0600

    Updated Username model
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/usernames/domain/username.dart
+++ lib/features/usernames/domain/username.dart
@@ -6,29 +6,32 @@ part 'username.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class Username {
-  Username({
-    this.id = '',
-    this.userId = '',
-    this.username = '',
-    this.description = '',
-    this.aliases = const <Alias>[],
-    this.defaultRecipient,
-    this.active = false,
-    this.catchAll = false,
+  const Username({
+    required this.id,
+    required this.userId,
+    required this.username,
+    required this.catchAll,
     required this.createdAt,
     required this.updatedAt,
+    required this.active,
+    this.description,
+    this.fromName,
+    this.aliases,
+    this.defaultRecipient,
+    this.canLogin,
   });
 
-  String id;
+  final String id;
 
   @JsonKey(name: 'user_id')
   final String userId;
-
   final String username;
+  final String? description;
 
-  final String description;
+  @JsonKey(name: 'from_name')
+  final String? fromName;
 
-  final List<Alias> aliases;
+  final List<Alias>? aliases;
 
   @JsonKey(name: 'default_recipient')
   final Recipient? defaultRecipient;
@@ -37,6 +40,9 @@ class Username {
 
   @JsonKey(name: 'catch_all')
   final bool catchAll;
+
+  @JsonKey(name: 'can_login')
+  final bool? canLogin;
 
   @JsonKey(name: 'created_at')
   final DateTime createdAt;
@@ -54,10 +60,12 @@ class Username {
     String? userId,
     String? username,
     String? description,
+    String? fromName,
     List<Alias>? aliases,
     Recipient? defaultRecipient,
     bool? active,
     bool? catchAll,
+    bool? canLogin,
     DateTime? createdAt,
     DateTime? updatedAt,
   }) {
@@ -66,10 +74,12 @@ class Username {
       userId: userId ?? this.userId,
       username: username ?? this.username,
       description: description ?? this.description,
+      fromName: fromName ?? this.fromName,
       aliases: aliases ?? this.aliases,
       defaultRecipient: defaultRecipient ?? this.defaultRecipient,
       active: active ?? this.active,
       catchAll: catchAll ?? this.catchAll,
+      canLogin: canLogin ?? this.canLogin,
       createdAt: createdAt ?? this.createdAt,
       updatedAt: updatedAt ?? this.updatedAt,
     );
@@ -77,6 +87,6 @@ class Username {
 
   @override
   String toString() {
-    return 'Username{id: $id, userId: $userId, username: $username, description: $description, aliases: $aliases, defaultRecipient: $defaultRecipient, active: $active, catchAll: $catchAll, createdAt: $createdAt, updatedAt: $updatedAt}';
+    return 'Username{id: $id, userId: $userId, username: $username, description: $description, fromName: $fromName, aliases: $aliases, defaultRecipient: $defaultRecipient, active: $active, catchAll: $catchAll, canLogin: $canLogin, createdAt: $createdAt, updatedAt: $updatedAt}';
   }
 }
