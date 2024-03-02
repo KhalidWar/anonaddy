34f64af81ae78734600ae36f7cbab07f137e6993
Khalid Warsame <dev@khalidwar.com>
2024-03-01 23:15:02 -0600

    Updated AliasDetailListTile
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/shared_components/list_tiles/alias_detail_list_tile.dart
+++ lib/shared_components/list_tiles/alias_detail_list_tile.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class AliasDetailListTile extends StatelessWidget {
   const AliasDetailListTile({
     Key? key,
+    required this.subtitle,
     this.trailingIconOnPress,
     this.trailingIconData,
-    this.subtitle,
     this.leadingIconData,
     this.title,
     this.trailing,
@@ -13,54 +13,55 @@ class AliasDetailListTile extends StatelessWidget {
     this.leadingIconColor,
   }) : super(key: key);
 
-  final Function? trailingIconOnPress;
+  final Function()? trailingIconOnPress;
   final IconData? trailingIconData, leadingIconData;
   final Color? leadingIconColor;
-  final dynamic title, subtitle;
+  final String? title;
+  final String subtitle;
   final Widget? trailing;
   final TextStyle? titleTextStyle;
 
   @override
   Widget build(BuildContext context) {
-    return title == null
-        ? Container()
-        : InkWell(
-            onTap: trailingIconOnPress as void Function()?,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-              child: Row(
+    if (title == null) return const SizedBox.shrink();
+
+    return InkWell(
+      onTap: trailingIconOnPress,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+        child: Row(
+          children: [
+            Icon(leadingIconData, color: leadingIconColor),
+            const SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(leadingIconData, color: leadingIconColor),
-                  const SizedBox(width: 15),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          title,
-                          style: titleTextStyle,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 2),
-                          child: Text(
-                            '$subtitle',
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                      ],
-                    ),
+                  Text(
+                    title!,
+                    style: titleTextStyle,
                   ),
-                  IgnorePointer(
-                    child: trailingIconData == null
-                        ? trailing ?? Container()
-                        : IconButton(
-                            icon: Icon(trailingIconData),
-                            onPressed: trailingIconOnPress as void Function()?,
-                          ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 2),
+                    child: Text(
+                      subtitle,
+                      style: const TextStyle(color: Colors.grey),
+                    ),
                   ),
                 ],
               ),
             ),
-          );
+            IgnorePointer(
+              child: trailingIconData == null
+                  ? trailing ?? Container()
+                  : IconButton(
+                      icon: Icon(trailingIconData),
+                      onPressed: trailingIconOnPress,
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
