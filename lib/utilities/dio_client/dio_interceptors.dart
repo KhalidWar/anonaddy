ab69aeb73513efe8d61799197845278155106b67
Khalid Warsame <dev@khalidwar.com>
2024-03-13 07:07:09 -0500

    Updated exception handling
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/utilities/dio_client/dio_interceptors.dart
+++ lib/utilities/dio_client/dio_interceptors.dart
@@ -40,71 +40,72 @@ class DioInterceptors extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
     switch (err.type) {
-      case DioExceptionType.badCertificate:
       case DioExceptionType.connectionError:
+        return handler.next(
+          err.copyWith(message: 'No internet connection, please try again.'),
+        );
       case DioExceptionType.connectionTimeout:
       case DioExceptionType.sendTimeout:
       case DioExceptionType.receiveTimeout:
-        throw 'Connection has timed out, please try again.';
+        return handler.next(
+          err.copyWith(message: 'Connection has timed out, please try again.'),
+        );
       case DioExceptionType.cancel:
-        break;
-      case DioExceptionType.unknown:
-        throw 'No internet connection, please try again.';
-
+        return handler.next(
+          err.copyWith(message: 'Request cancelled'),
+        );
       case DioExceptionType.badResponse:
-        switch (err.response?.statusCode) {
-          case 400:
-            throw _responseError(err, 'Bad Request -- Your request sucks');
-          case 401:
-            throw _responseError(
-              err,
-              'Unauthenticated -- Your API key is wrong',
-            );
-          case 403:
-            throw _responseError(
-              err,
-              'Forbidden -- You do not have permission to access the requested resource',
-            );
-          case 404:
-            throw _responseError(
-              err,
-              'Not Found -- The specified resource could not be found',
-            );
-          case 405:
-            throw _responseError(
-              err,
-              'Method Not Allowed -- You tried to access an endpoint with an invalid method',
-            );
-          case 422:
-            throw _responseError(
-                err, 'Validation Error -- The given data was invalid');
-          case 429:
-            throw _responseError(
-              err,
-              'Too Many Requests -- You\'re sending too many requests or have reached your limit for new aliases',
-            );
-          case 500:
-            throw _responseError(
-              err,
-              'Internal Server Error -- We had a problem with our server. Try again later',
-            );
-          case 503:
-            throw _responseError(
-              err,
-              'Service Unavailable -- We\'re temporarily offline for maintenance. Please try again later',
-            );
-        }
-        break;
+        return handler.next(err.stringifyStatusCode);
+      case DioExceptionType.badCertificate:
+      case DioExceptionType.unknown:
+        return handler.next(
+          err.copyWith(message: 'Something went wrong, please try again.'),
+        );
     }
-
-    return handler.next(err);
   }
+}
 
-  DioException _responseError(DioException err, String message) {
-    return DioException(
-      type: err.type,
-      requestOptions: err.requestOptions,
-      error: message,
-    );
+extension DioExceptionExtension on DioException {
+  DioException get stringifyStatusCode {
+    switch (response?.statusCode) {
+      case 400:
+        return copyWith(message: 'Bad Request -- Your request sucks');
+      case 401:
+        return copyWith(message: 'Unauthenticated -- Your API key is wrong');
+      case 403:
+        return copyWith(
+          message:
+              'Forbidden -- You do not have permission to access the requested resource',
+        );
+      case 404:
+        return copyWith(
+          message: 'Not Found -- The specified resource could not be found',
+        );
+      case 405:
+        return copyWith(
+          message:
+              'Method Not Allowed -- You tried to access an endpoint with an invalid method',
+        );
+      case 422:
+        return copyWith(
+          message: 'Validation Error -- The given data was invalid',
+        );
+      case 429:
+        return copyWith(
+          message:
+              'Too Many Requests -- You\'re sending too many requests or have reached your limit for new aliases',
+        );
+      case 500:
+        return copyWith(
+          message:
+              'Internal Server Error -- We had a problem with our server. Try again later',
+        );
+      case 503:
+        return copyWith(
+          message:
+              'Service Unavailable -- We\'re temporarily offline for maintenance. Please try again later',
+        );
+    }
+    return copyWith(message: 'Something went wrong, please try again.');
   }
 }
