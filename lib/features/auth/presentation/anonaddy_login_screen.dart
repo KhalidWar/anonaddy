87c37f4186bc374423bb6368f83713419caa4a12
Khalid Warsame <dev@khalidwar.com>
2024-03-29 07:40:46 -0500

    Updated default theme
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/auth/presentation/anonaddy_login_screen.dart
+++ lib/features/auth/presentation/anonaddy_login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:anonaddy/features/auth/presentation/components/access_token_info.dart';
 import 'package:anonaddy/features/auth/presentation/components/login_card.dart';
 import 'package:anonaddy/features/auth/presentation/controller/auth_notifier.dart';
+import 'package:anonaddy/shared_components/constants/app_colors.dart';
 import 'package:anonaddy/shared_components/constants/app_strings.dart';
 import 'package:anonaddy/shared_components/constants/url_strings.dart';
 import 'package:anonaddy/utilities/form_validator.dart';
+import 'package:anonaddy/utilities/theme.dart';
 import 'package:anonaddy/utilities/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,113 +35,114 @@ class _AddyLoginScreenState extends ConsumerState<AddyLoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-      child: Scaffold(
-        key: const Key('loginScreenScaffold'),
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Theme.of(context).primaryColor,
-        body: Center(
-          child: LoginCard(
-            footerOnPress: login,
-            child: Column(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Login with Access Token',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    const SizedBox(height: 4),
-                    Form(
-                      key: _tokenFormKey,
-                      child: TextFormField(
-                        key: const Key('loginScreenTextField'),
-                        validator: FormValidator.requiredField,
-                        onChanged: (input) => _token = input,
-                        onFieldSubmitted: (input) => login(),
-                        textInputAction: TextInputAction.go,
-                        keyboardType: TextInputType.multiline,
-                        minLines: 6,
-                        maxLines: 7,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: AppStrings.enterAccessToken,
+    return Theme(
+      data: AppTheme.light,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Scaffold(
+          key: const Key('loginScreenScaffold'),
+          resizeToAvoidBottomInset: false,
+          backgroundColor: AppColors.primaryColor,
+          body: Center(
+            child: LoginCard(
+              footerOnPress: login,
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Login with Access Token'),
+                      const SizedBox(height: 4),
+                      Form(
+                        key: _tokenFormKey,
+                        child: TextFormField(
+                          key: const Key('loginScreenTextField'),
+                          validator: FormValidator.requiredField,
+                          onChanged: (input) => _token = input,
+                          onFieldSubmitted: (input) => login(),
+                          textInputAction: TextInputAction.go,
+                          keyboardType: TextInputType.multiline,
+                          minLines: 6,
+                          maxLines: 7,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: AppStrings.enterAccessToken,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextButton(
-                      key: const Key('loginScreenAccessTokenInfoButton'),
-                      child: const Text(AppStrings.whatsAccessToken),
-                      onPressed: () async {
-                        await WoltModalSheet.show(
-                          context: context,
-                          onModalDismissedWithBarrierTap:
-                              Navigator.of(context).pop,
-                          pageListBuilder: (context) {
-                            return [
-                              Utilities.buildWoltModalSheetSubPage(
-                                context,
-                                topBarTitle: AppStrings.whatsAccessToken,
-                                child: const AccessTokenInfo(),
-                                sabGradientColor: Colors.transparent,
-                                stickyActionBar: Container(
-                                  height: 72,
-                                  width: double.infinity,
-                                  padding: const EdgeInsets.all(16),
-                                  child: ElevatedButton.icon(
-                                    icon: const Icon(
-                                      Icons.open_in_new_outlined,
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextButton(
+                        key: const Key('loginScreenAccessTokenInfoButton'),
+                        child: const Text(AppStrings.whatsAccessToken),
+                        onPressed: () async {
+                          await WoltModalSheet.show(
+                            context: context,
+                            onModalDismissedWithBarrierTap:
+                                Navigator.of(context).pop,
+                            pageListBuilder: (context) {
+                              return [
+                                Utilities.buildWoltModalSheetSubPage(
+                                  context,
+                                  topBarTitle: AppStrings.whatsAccessToken,
+                                  child: const AccessTokenInfo(),
+                                  sabGradientColor: Colors.transparent,
+                                  stickyActionBar: Container(
+                                    height: 72,
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(16),
+                                    child: ElevatedButton.icon(
+                                      icon: const Icon(
+                                        Icons.open_in_new_outlined,
+                                      ),
+                                      label: const Text(
+                                        AppStrings.getAccessToken,
+                                      ),
+                                      onPressed: () {
+                                        Utilities.launchURL(
+                                            kAnonAddySettingsURL);
+                                      },
                                     ),
-                                    label: const Text(
-                                      AppStrings.getAccessToken,
-                                    ),
-                                    onPressed: () {
-                                      Utilities.launchURL(kAnonAddySettingsURL);
-                                    },
                                   ),
                                 ),
-                              ),
-                            ];
-                          },
-                        );
-                      },
-                    ),
-                    TextButton(
-                      key: const Key('loginScreenChangeInstanceButton'),
-                      onPressed: ref
-                          .read(authStateNotifier.notifier)
-                          .goToSelfHostedLogin,
-                      child: const Text('Self Hosted? Change Instance!'),
-                    ),
-                    // TextButton(
-                    //   child: const Text('Log in with email and password'),
-                    //   onPressed: () async {
-                    //     await WoltModalSheet.show(
-                    //       context: context,
-                    //       pageListBuilder: (context) {
-                    //         return [
-                    //           Utilities.buildWoltModalSheetSubPage(
-                    //             context,
-                    //             topBarTitle: 'Username & Password Login',
-                    //             child: const UsernamePasswordLoginScreen(),
-                    //           ),
-                    //         ];
-                    //       },
-                    //     );
-                    //   },
-                    // ),
-                  ],
-                ),
-              ],
+                              ];
+                            },
+                          );
+                        },
+                      ),
+                      TextButton(
+                        key: const Key('loginScreenChangeInstanceButton'),
+                        onPressed: ref
+                            .read(authStateNotifier.notifier)
+                            .goToSelfHostedLogin,
+                        child: const Text('Self Hosted? Change Instance!'),
+                      ),
+                      // TextButton(
+                      //   child: const Text('Log in with email and password'),
+                      //   onPressed: () async {
+                      //     await WoltModalSheet.show(
+                      //       context: context,
+                      //       pageListBuilder: (context) {
+                      //         return [
+                      //           Utilities.buildWoltModalSheetSubPage(
+                      //             context,
+                      //             topBarTitle: 'Username & Password Login',
+                      //             child: const UsernamePasswordLoginScreen(),
+                      //           ),
+                      //         ];
+                      //       },
+                      //     );
+                      //   },
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/features/auth/presentation/self_host_login_screen.dart
+++ lib/features/auth/presentation/self_host_login_screen.dart
@@ -5,6 +5,7 @@ import 'package:anonaddy/shared_components/constants/app_strings.dart';
 import 'package:anonaddy/shared_components/constants/app_url.dart';
 import 'package:anonaddy/shared_components/constants/url_strings.dart';
 import 'package:anonaddy/utilities/form_validator.dart';
+import 'package:anonaddy/utilities/theme.dart';
 import 'package:anonaddy/utilities/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -36,110 +37,104 @@ class _SelfHostLoginScreenState extends ConsumerState<SelfHostLoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-      child: Scaffold(
-        key: const Key('selfHostedLoginScreenScaffold'),
-        resizeToAvoidBottomInset: false,
-        backgroundColor: AppColors.primaryColor,
-        body: Center(
-          child: LoginCard(
-            footerOnPress: login,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'addy.io Instance',
-                      key: const Key('selfHostedLoginScreenUrlInputLabel'),
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    const SizedBox(height: 4),
-                    Form(
-                      key: _urlFormKey,
-                      child: TextFormField(
-                        key: const Key('selfHostedLoginScreenUrlInputField'),
-                        validator: (input) =>
-                            FormValidator.validateInstanceURL(input!),
-                        onChanged: (input) => _url = input,
-                        textInputAction: TextInputAction.next,
-                        keyboardType: TextInputType.multiline,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.secondary,
+    return Theme(
+      data: AppTheme.light,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Scaffold(
+          key: const Key('selfHostedLoginScreenScaffold'),
+          resizeToAvoidBottomInset: false,
+          backgroundColor: AppColors.primaryColor,
+          body: Center(
+            child: LoginCard(
+              footerOnPress: login,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Addy.io Instance',
+                        key: Key('selfHostedLoginScreenUrlInputLabel'),
+                      ),
+                      const SizedBox(height: 4),
+                      Form(
+                        key: _urlFormKey,
+                        child: TextFormField(
+                          key: const Key('selfHostedLoginScreenUrlInputField'),
+                          validator: (input) =>
+                              FormValidator.validateInstanceURL(input!),
+                          onChanged: (input) => _url = input,
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.multiline,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
                             ),
+                            border: const OutlineInputBorder(),
+                            hintText: AppUrl.anonAddyAuthority,
                           ),
-                          border: const OutlineInputBorder(),
-                          hintText: AppUrl.anonAddyAuthority,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'API Token ',
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Access Token ',
+                        key: Key('selfHostedLoginScreenTokenInputLabel'),
+                      ),
+                      const SizedBox(height: 4),
+                      Form(
+                        key: _tokenFormKey,
+                        child: TextFormField(
                           key:
-                              const Key('selfHostedLoginScreenTokenInputLabel'),
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        Text(
-                          '(from the settings page)',
-                          style: Theme.of(context).textTheme.bodySmall,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 4),
-                    Form(
-                      key: _tokenFormKey,
-                      child: TextFormField(
-                        key: const Key('selfHostedLoginScreenTokenInputField'),
-                        validator: FormValidator.requiredField,
-                        onChanged: (input) => _token = input,
-                        onFieldSubmitted: (input) => login(),
-                        textInputAction: TextInputAction.done,
-                        keyboardType: TextInputType.multiline,
-                        minLines: 3,
-                        maxLines: 4,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.secondary,
+                              const Key('selfHostedLoginScreenTokenInputField'),
+                          validator: FormValidator.requiredField,
+                          onChanged: (input) => _token = input,
+                          onFieldSubmitted: (input) => login(),
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.multiline,
+                          minLines: 3,
+                          maxLines: 4,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
                             ),
+                            border: const OutlineInputBorder(),
+                            hintText: AppStrings.enterYourApiToken,
                           ),
-                          border: const OutlineInputBorder(),
-                          hintText: AppStrings.enterYourApiToken,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    TextButton(
-                      key: const Key('selfHostedLoginScreenSelfHostInfoButton'),
-                      child: const Text('How to self-host addy.io?'),
-                      onPressed: () =>
-                          Utilities.launchURL(kAnonAddySelfHostingURL),
-                    ),
-                    TextButton(
-                      key:
-                          const Key('selfHostedLoginScreenAnonAddyLoginButton'),
-                      onPressed:
-                          ref.read(authStateNotifier.notifier).goToAddyLogin,
-                      child: const Text('Login with addy.io instead!'),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      TextButton(
+                        key: const Key(
+                            'selfHostedLoginScreenSelfHostInfoButton'),
+                        child: const Text('How to self-host addy.io?'),
+                        onPressed: () =>
+                            Utilities.launchURL(kAnonAddySelfHostingURL),
+                      ),
+                      TextButton(
+                        key: const Key(
+                            'selfHostedLoginScreenAnonAddyLoginButton'),
+                        onPressed:
+                            ref.read(authStateNotifier.notifier).goToAddyLogin,
+                        child: const Text('Login with addy.io instead!'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
