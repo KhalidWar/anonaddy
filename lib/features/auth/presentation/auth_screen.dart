70126557df17841fe67052da3a6326d5b467f3a5
Khalid Warsame <dev@khalidwar.com>
2024-03-29 06:50:08 -0500

    Removed MacOS screens
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/auth/presentation/auth_screen.dart
+++ lib/features/auth/presentation/auth_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:anonaddy/features/auth/presentation/anonaddy_login_screen.dart';
 import 'package:anonaddy/features/auth/presentation/controller/auth_notifier.dart';
 import 'package:anonaddy/features/auth/presentation/controller/auth_state.dart';
@@ -8,7 +6,6 @@ import 'package:anonaddy/features/auth/presentation/loading_screen.dart';
 import 'package:anonaddy/features/auth/presentation/lock_screen.dart';
 import 'package:anonaddy/features/auth/presentation/self_host_login_screen.dart';
 import 'package:anonaddy/features/home/presentation/home_screen.dart';
-import 'package:anonaddy/macos/macos_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:secure_application/secure_application.dart';
@@ -41,8 +38,6 @@ class AuthScreen extends ConsumerWidget {
           ///
           /// Manages when a logged in user is found
           case AuthorizationStatus.authorized:
-            if (Platform.isMacOS) return const MacosScreen();
-
             switch (authState.authenticationStatus) {
               case AuthenticationStatus.enabled:
 
