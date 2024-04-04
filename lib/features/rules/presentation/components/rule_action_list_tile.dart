cf42eae29eef79ec1b4b02f46e77d5dfe26b3db9
Khalid Warsame <dev@khalidwar.com>
2024-04-04 16:57:07 -0500

    Added RuleCheckbox
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

---
+++ lib/features/rules/presentation/components/rule_action_list_tile.dart
@@ -0,0 +1,101 @@
+import 'package:anonaddy/features/rules/domain/rule_action.dart';
+import 'package:flutter/material.dart';
+
+class RuleActionListTile extends StatelessWidget {
+  const RuleActionListTile({
+    super.key,
+    required this.action,
+    required this.isFirst,
+    required this.onPress,
+  });
+
+  final RuleAction action;
+  final bool isFirst;
+  final Function() onPress;
+
+  Widget buildValue(BuildContext context) {
+    switch ((action.runtimeType)) {
+      case RuleActionSubject:
+        final subject = action as RuleActionSubject;
+        return Chip(
+          label: Text(subject.value),
+          visualDensity: VisualDensity.compact,
+          backgroundColor: Theme.of(context).cardColor,
+          padding: const EdgeInsets.all(0),
+          labelPadding: const EdgeInsets.symmetric(horizontal: 4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+        );
+      case RuleActionDisplayFrom:
+        final displayFrom = action as RuleActionDisplayFrom;
+        return Chip(
+          label: Text(displayFrom.value),
+          visualDensity: VisualDensity.compact,
+          backgroundColor: Theme.of(context).cardColor,
+          padding: const EdgeInsets.all(0),
+          labelPadding: const EdgeInsets.symmetric(horizontal: 4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+        );
+      case RuleActionBanner:
+        final banner = action as RuleActionBanner;
+        return Chip(
+          label: Text(banner.bannerLocation.value),
+          backgroundColor: Theme.of(context).cardColor,
+          visualDensity: VisualDensity.compact,
+          padding: const EdgeInsets.all(0),
+          labelPadding: const EdgeInsets.symmetric(horizontal: 4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+        );
+      default:
+        return const SizedBox.shrink();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(color: Theme.of(context).colorScheme.primary),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(isFirst ? 'Then ' : 'And then '),
+                      Text(
+                        '${action.type.value} ',
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  buildValue(context),
+                ],
+              ),
+            ),
+            // const Spacer(),
+            const Icon(Icons.arrow_forward_ios_rounded),
+          ],
+        ),
+      ),
+    );
+  }
+}
