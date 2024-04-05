77124c287a4120f2914884b4f1377d357e0c22bd
Khalid Warsame <dev@khalidwar.com>
2024-04-05 16:44:08 -0500

    Updated RuleCheckbox UI
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/rules/presentation/components/rule_checkbox.dart
+++ lib/features/rules/presentation/components/rule_checkbox.dart
@@ -14,22 +14,11 @@ class RuleCheckbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onChanged(!value),
-      child: Row(
-        children: [
-          IgnorePointer(
-            child: Checkbox.adaptive(
-              value: value,
-              onChanged: onChanged,
-            ),
-          ),
-          Text(
-            title,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-        ],
-      ),
+    return FilterChip(
+      selected: value,
+      labelPadding: const EdgeInsets.all(0),
+      label: Text(title),
+      onSelected: onChanged,
     );
   }
 }
