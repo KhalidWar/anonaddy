74915d9e21519e7e458b6b912ff2d402cf424221
Khalid Warsame <dev@khalidwar.com>
2024-04-09 07:13:16 -0500

    Added OnboardingPages
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

---
+++ lib/features/onboarding/presentation/onboarding_pages.dart
@@ -0,0 +1,40 @@
+enum OnboardingPages {
+  account(
+    'assets/images/domains.svg',
+    'Your Account. Your Way.',
+    'With full offline support, access and manage your Addy.io (formerly AnonAddy) account, aliases, recipients, domains, and additional usernames from anywhere, any time.',
+  ),
+  aliases(
+    'assets/images/aliases.svg',
+    'Create Aliases',
+    'Either on the fly or generated beforehand. Next time you are signing up to a website or newsletter, simply make up a new alias and enter that instead of your real email address.',
+  ),
+  recipients(
+    'assets/images/recipients.svg',
+    'Recipients',
+    'Manage and easily add multiple recipients to go with new or existing aliases, as well as add your own GPG/OpenPGP public keys to recipients.',
+  ),
+  addyManager(
+    'assets/images/api.svg',
+    'Addy.io for mobile',
+    'AddyManager is a standalone app independently built and maintained to help you access and manage your Addy.io account, and is not associated with the official Addy.io project or team.',
+    showAddyManagerLinks: true,
+  ),
+  login(
+    'assets/images/register.svg',
+    'Multiple Login Options',
+    'Anonymous',
+  );
+
+  const OnboardingPages(
+    this.path,
+    this.title,
+    this.subtitle, {
+    this.showAddyManagerLinks = false,
+  });
+
+  final String path;
+  final String title;
+  final String subtitle;
+  final bool showAddyManagerLinks;
+}
