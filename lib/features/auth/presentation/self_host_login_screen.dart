e4d96dec330b130906afaa2190dca49e5defdebc
Khalid Warsame <dev@khalidwar.com>
2024-04-10 20:06:32 -0500

    Fixed text visibility
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/auth/presentation/self_host_login_screen.dart
+++ lib/features/auth/presentation/self_host_login_screen.dart
@@ -3,7 +3,6 @@ import 'package:anonaddy/shared_components/constants/app_strings.dart';
 import 'package:anonaddy/shared_components/constants/app_url.dart';
 import 'package:anonaddy/shared_components/platform_aware_widgets/platform_button.dart';
 import 'package:anonaddy/utilities/form_validator.dart';
-import 'package:anonaddy/utilities/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -30,103 +29,100 @@ class _SelfHostLoginScreenState extends ConsumerState<SelfHostLoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: AppTheme.light,
-      child: GestureDetector(
-        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-        key: const Key('selfHostedLoginScreenScaffold'),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Form(
-                key: formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Your Instance Url',
-                      key: Key('selfHostedLoginScreenUrlInputLabel'),
-                    ),
-                    const SizedBox(height: 4),
-                    TextFormField(
-                      key: const Key('selfHostedLoginScreenUrlInputField'),
-                      validator: FormValidator.requiredField,
-                      onChanged: (input) => _url = input,
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                        border: const OutlineInputBorder(),
-                        hintText: AppUrl.anonAddyAuthority,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'Access Token ',
-                      key: Key('selfHostedLoginScreenTokenInputLabel'),
-                    ),
-                    const SizedBox(height: 4),
-                    TextFormField(
-                      key: const Key('selfHostedLoginScreenTokenInputField'),
-                      validator: FormValidator.requiredField,
-                      onChanged: (input) => _token = input,
-                      onFieldSubmitted: (input) => login(),
-                      textInputAction: TextInputAction.done,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                        border: const OutlineInputBorder(),
-                        hintText: AppStrings.enterYourApiToken,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 24),
-              // TextButton(
-              //   key: const Key('selfHostedLoginScreenSelfHostInfoButton'),
-              //   child: const Text('How to self-host addy.io?'),
-              //   onPressed: () => Utilities.launchURL(kAnonAddySelfHostingURL),
-              // ),
-              SizedBox(
-                width: double.infinity,
-                child: PlatformButton(
-                  key: const Key('loginFooterLoginButton'),
-                  onPress: login,
-                  child: Consumer(
-                    builder: (context, ref, _) {
-                      final authAsync = ref.watch(authStateNotifier);
-                      return authAsync.when(
-                        data: (authState) {
-                          return authState.loginLoading
-                              ? const CircularProgressIndicator(
-                                  key: Key('loginFooterLoginButtonLoading'),
-                                )
-                              : Text(
-                                  'Log in',
-                                  key: const Key('loginFooterLoginButtonLabel'),
-                                  style:
-                                      Theme.of(context).textTheme.headlineSmall,
-                                );
-                        },
-                        error: (_, __) => const SizedBox.shrink(),
-                        loading: () => const SizedBox.shrink(),
-                      );
-                    },
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+      key: const Key('selfHostedLoginScreenScaffold'),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Your Instance Url',
+                    key: Key('selfHostedLoginScreenUrlInputLabel'),
                   ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    key: const Key('selfHostedLoginScreenUrlInputField'),
+                    validator: FormValidator.requiredField,
+                    onChanged: (input) => _url = input,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      border: const OutlineInputBorder(),
+                      hintText: AppUrl.anonAddyAuthority,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Access Token ',
+                    key: Key('selfHostedLoginScreenTokenInputLabel'),
+                  ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    key: const Key('selfHostedLoginScreenTokenInputField'),
+                    validator: FormValidator.requiredField,
+                    onChanged: (input) => _token = input,
+                    onFieldSubmitted: (input) => login(),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      border: const OutlineInputBorder(),
+                      hintText: AppStrings.enterYourApiToken,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            // TextButton(
+            //   key: const Key('selfHostedLoginScreenSelfHostInfoButton'),
+            //   child: const Text('How to self-host addy.io?'),
+            //   onPressed: () => Utilities.launchURL(kAnonAddySelfHostingURL),
+            // ),
+            SizedBox(
+              width: double.infinity,
+              child: PlatformButton(
+                key: const Key('loginFooterLoginButton'),
+                onPress: login,
+                child: Consumer(
+                  builder: (context, ref, _) {
+                    final authAsync = ref.watch(authStateNotifier);
+                    return authAsync.when(
+                      data: (authState) {
+                        return authState.loginLoading
+                            ? const CircularProgressIndicator(
+                                key: Key('loginFooterLoginButtonLoading'),
+                              )
+                            : Text('Log in',
+                                key: const Key('loginFooterLoginButtonLabel'),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(color: Colors.black));
+                      },
+                      error: (_, __) => const SizedBox.shrink(),
+                      loading: () => const SizedBox.shrink(),
+                    );
+                  },
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
