1bccff87b6724b075d3dd1df173ed1aae6d3d446
Khalid Warsame <dev@khalidwar.com>
2024-04-17 17:30:18 -0500

    Removed unused code
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/alert_center/data/local_notification_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final localNotificationServiceProvider =
-    Provider((ref) => LocalNotificationService());
-
-class LocalNotificationService {}
