1eb2bfa659ecdb4cb3e8ce06eb1ee83e68382e19
Khalid Warsame <dev@khalidwar.com>
2024-04-23 21:29:25 -0500

    Removed unused code
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/features/connectivity/presentation/controller/connectivity_notifier.dart
+++ lib/features/connectivity/presentation/controller/connectivity_notifier.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final _connectivityServiceProvider = Provider((ref) => Connectivity());
@@ -11,23 +10,10 @@ final connectivityNotifierProvider =
         ConnectivityNotifier.new);
 
 class ConnectivityNotifier extends StreamNotifier<ConnectivityResult> {
-  Future<ConnectivityResult> initConnectivity() async {
-    try {
-      return await ref.read(_connectivityServiceProvider).checkConnectivity();
-    } on PlatformException catch (_) {
-      return ConnectivityResult.other;
-    } catch (e) {
-      return ConnectivityResult.other;
-    }
-  }
-
   @override
   Stream<ConnectivityResult> build() async* {
     final connectivity = ref.read(_connectivityServiceProvider);
     final stream = StreamController<ConnectivityResult>();
-
-    // final initialConnectivityResult = await initConnectivity();
-    // stream.add(initialConnectivityResult);
 
     connectivity.onConnectivityChanged.listen((connectivityResult) {
       stream.add(connectivityResult);
