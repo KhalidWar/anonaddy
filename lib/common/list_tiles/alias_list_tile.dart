db82a603f13cee5d7bf4c0b80a50cf9682483015
Khalid Warsame <dev@khalidwar.com>
2024-05-01 07:16:29 -0500

    Updated AliasListTile UI
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/common/list_tiles/alias_list_tile.dart
+++ lib/common/list_tiles/alias_list_tile.dart
@@ -1,74 +1,96 @@
+import 'package:anonaddy/common/constants/anonaddy_string.dart';
 import 'package:anonaddy/common/list_tiles/alias_list_tile_leading.dart';
+import 'package:anonaddy/common/platform_aware_widgets/dialogs/platform_alert_dialog.dart';
+import 'package:anonaddy/common/platform_aware_widgets/platform_aware.dart';
 import 'package:anonaddy/common/utilities.dart';
+import 'package:anonaddy/features/aliases/data/alias_screen_service.dart';
 import 'package:anonaddy/features/aliases/domain/alias.dart';
 import 'package:anonaddy/features/aliases/presentation/alias_screen.dart';
+import 'package:anonaddy/features/aliases/presentation/controller/available_aliases_notifier.dart';
+import 'package:anonaddy/features/aliases/presentation/controller/deleted_aliases_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AliasListTile extends StatelessWidget {
+class AliasListTile extends ConsumerWidget {
   const AliasListTile({
     super.key,
     required this.alias,
+    this.showDeleteRestore = false,
   });
 
   final Alias alias;
+  final bool showDeleteRestore;
+
+  Future<void> restoreAlias(WidgetRef ref) async {
+    await ref
+        .read(aliasScreenServiceProvider)
+        .restoreAlias(alias.id)
+        .whenComplete(ref
+            .read(deletedAliasesNotifierProvider.notifier)
+            .fetchDeletedAliases)
+        .catchError((e) => Utilities.showToast(e.toString()));
+  }
+
+  Future<void> deletedAlias(WidgetRef ref) async {
+    await ref
+        .read(aliasScreenServiceProvider)
+        .deleteAlias(alias.id)
+        .whenComplete(ref.read(aliasesNotifierProvider.notifier).fetchAliases)
+        .catchError((e) => Utilities.showToast(e.toString()));
+  }
 
   @override
-  Widget build(BuildContext context) {
-    final isDark = Theme.of(context).brightness == Brightness.dark;
-
-    return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
-          children: [
-            AliasListTileLeading(
-              isDeleted: alias.isDeleted,
-              isActive: alias.active,
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    alias.email,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: alias.isDeleted
-                              ? Colors.grey
-                              : isDark
-                                  ? Colors.white
-                                  : Colors.black,
-                        ),
-                  ),
-                  Text(
-                    alias.description.isEmpty
-                        ? 'No description'
-                        : alias.description,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-            ),
-            IconButton(
-              icon: const Icon(Icons.copy),
-              onPressed: alias.isDeleted
-                  ? null
-                  : () => Utilities.copyOnTap(alias.email),
-            ),
-          ],
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      dense: true,
+      minVerticalPadding: 0,
+      horizontalTitleGap: 12,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+      leading: AliasListTileLeading(
+        isDeleted: alias.isDeleted,
+        isActive: alias.active,
       ),
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          AliasScreen.routeName,
-          arguments: alias.id,
-        );
-      },
+      title: Text(alias.email),
+      subtitle: Text(
+        alias.description.isEmpty ? 'No description' : alias.description,
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (showDeleteRestore)
+            IconButton(
+              visualDensity: VisualDensity.compact,
+              icon:
+                  Icon(alias.isDeleted ? Icons.restore : Icons.delete_outline),
+              onPressed: () async {
+                PlatformAware.platformDialog(
+                  context: context,
+                  child: PlatformAlertDialog(
+                    title: '${alias.isDeleted ? 'Restore' : 'Delete'} Alias',
+                    content: alias.isDeleted
+                        ? AddyString.restoreAliasConfirmation
+                        : AddyString.deleteAliasConfirmation,
+                    method: () async {
+                      Navigator.pop(context);
+                      alias.isDeleted
+                          ? await restoreAlias(ref)
+                          : await deletedAlias(ref);
+                    },
+                  ),
+                );
+              },
+            ),
+          IconButton(
+            icon: const Icon(Icons.copy),
+            onPressed: () => Utilities.copyOnTap(alias.email),
+          ),
+        ],
+      ),
+      onTap: () => Navigator.pushNamed(
+        context,
+        AliasScreen.routeName,
+        arguments: alias.id,
+      ),
     );
   }
 }
