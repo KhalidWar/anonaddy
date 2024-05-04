2546f2077a432272c6ed07d380e949523e2e7da7
Khalid Warsame <dev@khalidwar.com>
2024-05-04 08:58:07 -0500

    Fixed failing test
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- test/features/aliases/presentation/alias_screen/alias_screen_test.dart
+++ test/features/aliases/presentation/alias_screen/alias_screen_test.dart
@@ -1,8 +1,8 @@
+import 'package:anonaddy/common/constants/app_strings.dart';
+import 'package:anonaddy/common/pie_chart/alias_screen_pie_chart.dart';
 import 'package:anonaddy/features/aliases/presentation/alias_screen.dart';
 import 'package:anonaddy/features/aliases/presentation/controller/alias_screen_notifier.dart';
 import 'package:anonaddy/features/aliases/presentation/controller/alias_screen_state.dart';
-import 'package:anonaddy/shared_components/constants/constants_exports.dart';
-import 'package:anonaddy/shared_components/pie_chart/alias_screen_pie_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
