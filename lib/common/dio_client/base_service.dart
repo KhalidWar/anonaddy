09427dbcb30064ad9e43c3e8bc6d66d52167ce58
Khalid Warsame <dev@khalidwar.com>
2024-06-27 19:33:38 -0500

    Simplified child/parent data
    Signed-off-by: Khalid Warsame <dev@khalidwar.com>

--- lib/common/dio_client/base_service.dart
+++ lib/common/dio_client/base_service.dart
@@ -2,21 +2,45 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:anonaddy/common/constants/app_strings.dart';
+import 'package:anonaddy/common/constants/data_storage_keys.dart';
+import 'package:anonaddy/features/aliases/presentation/alias_screen.dart';
+import 'package:anonaddy/features/app_version/domain/app_version.dart';
+import 'package:anonaddy/features/domain_options/domain/domain_options.dart';
+import 'package:anonaddy/features/domains/presentation/domain_screen.dart';
+import 'package:anonaddy/features/recipients/presentation/recipients_screen.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+/// Base class for all services.
+///
+/// Every service class should extend this class to access HTTP methods
+/// and data storage along with solid error handling and logging.
 abstract class BaseService {
   const BaseService({
     required this.dio,
     required this.secureStorage,
-    required this.storageKey,
-    this.parentStorageKey,
-  }) : assert(!(storageKey == '' && parentStorageKey == null));
+    this.storageKey,
+    this.isDetailedScreen = false,
+  });
 
   final Dio dio;
   final FlutterSecureStorage secureStorage;
-  final String storageKey;
-  final String? parentStorageKey;
+
+  /// Key used to store API response in [secureStorage] for offline use which
+  /// consists of a list of data such as [Alias], [Recipient], [Domain], etc.
+  ///
+  /// Every service class should provide a unique key defined in [DataStorageKeys].
+  ///
+  /// If null, data will not be stored which is okay when dealing with
+  /// data that isn't needed when offline such as:
+  ///   1. detailed screen: [AliasScreen], [DomainScreen], [RecipientsScreen], etc.
+  ///   2. temporary data: [DomainOptions], [AppVersion].
+  final String? storageKey;
+
+  /// If true, data will not be stored in [secureStorage] for detailed screens.
+  /// Instead, data will be fetched from the server every time the screen is opened.
+  /// When offline, data will be fetched from [secureStorage] if available.
+  final bool isDetailedScreen;
 
   Future<Map<String, dynamic>> get(
     String path, {
@@ -27,9 +51,12 @@ abstract class BaseService {
       final response = await dio.get(path, queryParameters: queryParameters);
       log('BaseService get($path): statusCode ${response.statusCode}');
 
-      final responseData = response.data;
-      if (storageKey.isNotEmpty) await _saveData(responseData);
-      return responseData;
+      /// If [storageKey] is provided, save the response data in [secureStorage].
+      if (!isDetailedScreen && storageKey != null) {
+        await _saveData(response.data);
+      }
+
+      return response.data;
     } on DioException catch (dioException) {
       if (dioException.type == DioExceptionType.connectionError) {
         final loadedData = await loadData(childId);
@@ -86,10 +113,12 @@ abstract class BaseService {
 
   Future<Map<String, dynamic>?> loadData([String? childId]) async {
     try {
+      if (storageKey == null) return null;
+
       final allData = await secureStorage.readAll();
 
-      if (parentStorageKey != null && childId != null) {
-        final parentData = allData[parentStorageKey];
+      if (childId != null) {
+        final parentData = allData[storageKey];
         if (parentData == null) return null;
 
         final decodedParentData = jsonDecode(parentData);
@@ -110,7 +139,7 @@ abstract class BaseService {
   Future<void> _saveData(Map<String, dynamic> data) async {
     try {
       final encodedData = jsonEncode(data);
-      await secureStorage.write(value: encodedData, key: storageKey);
+      await secureStorage.write(value: encodedData, key: storageKey!);
     } catch (_) {
       return;
     }
